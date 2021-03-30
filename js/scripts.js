3af13aa4831f5cea40036a2d0f01b64ec996b807
Fernando Valani Cavalcante <valanife@gmail.com>
2021-03-30 19:36:33 -0300

    esboço de js

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function selecionarprato(){
+    var select = 10;
+    alert(select);
+}
+
+function selecionarbebida(){
+    var selectb = 20;
+    alert(selectb);
+}
+
+function selecionarsobremesa(){
+    var selects = 30;
+    alert(selects);
+}
+
+function Total(){
+    let total = valorsobremesa + valorbebida + valorprato;
+    return(prato, valorprato, bebida, valorbebida, sobremesa, valorsobremesa, "Total", total);
+}
