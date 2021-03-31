1b635b84583dd7b998860d915274b741faa01702
Fernando Valani Cavalcante <valanife@gmail.com>
2021-03-31 20:11:15 -0300

    tentativa de js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
-function selecionarprato(){
-    var select = 10;
-    alert(select);
+function selecionarPrato1(){
+    const selecionado = document.querySelector(".prato");
+    const icone = document.querySelector(".preco ion-icon");
+    selecionado.classList.toggle('selecionado');
+    icone.classList.toggle('desaparecer');
+
+}
+function selecionarPrato2(){
+    const selecionado = document.querySelector(".prato:nth-child(2)");
+    const icone = document.querySelector(".preco ion-icon");
+    selecionado.classList.toggle('selecionado');
+    icone.classList.toggle('desaparecer');
+
+}function selecionarPrato3(){
+    const selecionado = document.querySelector(".prato:nth-child(3)");
+    const icone = document.querySelector(".preco ion-icon");
+    selecionado.classList.toggle('selecionado');
+    icone.classList.toggle('desaparecer');
+
+}function selecionarPrato4(){
+    const selecionado = document.querySelector(".prato:last-child");
+    const icone = document.querySelector(".preco ion-icon");
+    selecionado.classList.toggle('selecionado');
+    icone.classList.toggle('desaparecer');
+
 }
 
 function selecionarbebida(){
