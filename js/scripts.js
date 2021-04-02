1c6127cb937dce859663f9141de0c404ac8b0664
Fernando Valani Cavalcante <valanife@gmail.com>
2021-04-02 18:31:23 -0300

    selecionar feito

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,36 @@ function selecionarPrato(prato){
     tique.classList.add('aparecer');
 }
 
-function selecionarbebida(){
-    var selectb = 20;
-    alert(selectb);
+function selecionarbebida(prato){
+    const selecionado = document.querySelector(".bebida .selecionado");
+    const icone = document.querySelector(".bebida .aparecer");
+
+    if (selecionado !== null){
+        selecionado.classList.remove('selecionado');
+        icone.classList.remove('aparecer');
+    }
+
+    const seletor = document.querySelector("." + prato);
+    seletor.classList.add('selecionado');
+
+    const tique = document.querySelector("."+ prato +" ion-icon");
+    tique.classList.add('aparecer');
 }
 
-function selecionarsobremesa(){
-    var selects = 30;
-    alert(selects);
+function selecionarsobremesa(prato){
+    const selecionado = document.querySelector(".sobremesa .selecionado");
+    const icone = document.querySelector(".sobremesa .aparecer");
+
+    if (selecionado !== null){
+        selecionado.classList.remove('selecionado');
+        icone.classList.remove('aparecer');
+    }
+
+    const seletor = document.querySelector("." + prato);
+    seletor.classList.add('selecionado');
+
+    const tique = document.querySelector("."+ prato +" ion-icon");
+    tique.classList.add('aparecer');
 }
 
 function Total(){
