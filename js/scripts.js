c3a05af812e606089c2a2933a0af5da4a136bf20
Fernando Valani Cavalcante <valanife@gmail.com>
2021-04-03 03:36:57 -0300

    cancelar pedido feito

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,10 @@ let precoPrato = 0;
 let precoBebida = 0;
 let precoSobremesa = 0;
 
+let nomePrato;
+let nomeBebida;
+let nomeSobremesa;
+
 function selecionarPrato(prato, nome, valor){
     const selecionado = document.querySelector(".pratos .selecionado");
     const icone = document.querySelector(".pratos .aparecer");
@@ -23,6 +27,7 @@ function selecionarPrato(prato, nome, valor){
 
     prin = 1;
     precoPrato = valor;
+    nomePrato = nome;
     fechar();
 }
 
@@ -43,6 +48,7 @@ function selecionarbebida(prato, nome, valor){
 
     sec = 1;
     precoBebida = valor;
+    nomeBebida = nome;
     fechar();
 }
 
@@ -63,6 +69,7 @@ function selecionarsobremesa(prato, nome, valor){
 
     ter = 1;
     precoSobremesa = valor;
+    nomeSobremesa = nome;
     fechar();
 }
 
@@ -76,4 +83,25 @@ function fechar(){
 function fecharPedido(){
     const pedido = document.querySelector(".confirmacao");
     pedido.classList.remove('desaparecer');
+
+    const prato = document.querySelector(".escolha-comida:firstchild");
+    prato.innerHTML = nomePrato;
+    const precoP = document.querySelector(".escolha-comida:nthchild(2)");
+    precoP.innerHTML = precoPrato;
+
+    const bebida = document.querySelector(".escolha-bebida span:firstchild");
+    bebida.innerHTML = nomeBebida;
+    const precoB = document.querySelector(".escolha-bebida span:nthchild(2)");
+    precoB.innerHTML = precoBebida;
+
+    const sobremesa = document.querySelector(".escolha-sobremesa span:firstchild");
+    sobremesa.innerHTML = nomeSobremesa;
+    const precoS = document.querySelector(".escolha-sobremesa span:nthchild(2)");
+    precoS.innerHTML = precoSobremesa;
+
+}
+
+function cancelarPedido(){
+    const pedido = document.querySelector(".confirmacao");
+    pedido.classList.add('desaparecer');
 }
